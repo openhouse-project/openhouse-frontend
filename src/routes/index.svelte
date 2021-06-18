ca37e34f558ca6a23cb2a603076423d3a66ca169
Andrew Ewing <drew@phenocode.com>
2021-06-18 14:50:00 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<div class="bg-gray-100 rounded pa-4">
+	<h1>Welcome to SvelteKit (+ TailwindCSS)</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
