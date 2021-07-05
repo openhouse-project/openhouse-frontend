3bb7a3e222b1b634c813c5d22c9620c674c7dfb9
Joseph Petrich <jpetrich@google.com>
2021-07-05 14:46:04 -0400

    Point tip address at new wallet avaliable on Polygon. Point to Polygon Openhouse contract

--- src/lib/contracts/openhouse.ts
+++ src/lib/contracts/openhouse.ts
@@ -1,5 +1,6 @@
 //export const OPENHOUSE_ADDRESS = '0xFf3D0372a03118cB6343EAEE7A7918e73982e872';	 // Rinkeby Testnet
-export const OPENHOUSE_ADDRESS = '0x96E1E0ad86b886D91B2A43Ae0061FE041950a5Bb'; // Ropsten Testnet
+//export const OPENHOUSE_ADDRESS = '0x96E1E0ad86b886D91B2A43Ae0061FE041950a5Bb'; // Ropsten Testnet
+export const OPENHOUSE_ADDRESS = '0x151A051FE0a9414950Ef0B34030294cEaB6F043a'; // Polygon
 export const OPENHOUSE_CONTRACT: any[] = [
 	{
 		"inputs": [
